1190bcbfa194a4abd308bf30cf3e688f28fc1b9c
Dorian <dobisike@cnm.edu>
2025-06-10 16:30:33 -0600

    Started CRUD inventory.

---
+++ app/src/main/java/edu/cnm/deepdive/notes/model/entity/Image.java
@@ -0,0 +1,68 @@
+package edu.cnm.deepdive.notes.model.entity;
+
+import android.net.Uri;
+import java.time.Instant;
+
+public class Image {
+
+  /*
+  C - Attach image to note
+  R - Retrieve all images for a note
+  U - Maybe update caption
+  D - Ability to delete image from note
+   */
+  private long id;
+  private String caption;
+  private String mimeType;
+  private Uri uri;
+  private Instant created;
+  private long noteId;
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getCaption() {
+    return caption;
+  }
+
+  public void setCaption(String caption) {
+    this.caption = caption;
+  }
+
+  public String getMimeType() {
+    return mimeType;
+  }
+
+  public void setMimeType(String mimeType) {
+    this.mimeType = mimeType;
+  }
+
+  public Uri getUri() {
+    return uri;
+  }
+
+  public void setUri(Uri uri) {
+    this.uri = uri;
+  }
+
+  public Instant getCreated() {
+    return created;
+  }
+
+  public void setCreated(Instant created) {
+    this.created = created;
+  }
+
+  public long getNoteId() {
+    return noteId;
+  }
+
+  public void setNoteId(long noteId) {
+    this.noteId = noteId;
+  }
+}
